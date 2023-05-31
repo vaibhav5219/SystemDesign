bdb713b184e169994ee46623743eadc04887b98d
Vaibhav Kumar <vaibhavkumar5219@gmail.com>
2023-05-31 09:45:28 +0530

    Initial Commit

---
+++ System Design/SOLID Principle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID_Principle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
