04ae1ffb7e855ec463199485b3fbeb227995a573
Vaibhav Kumar <vaibhavkumar5219@gmail.com>
2023-05-31 10:03:41 +0530

    Single Responsibility
    Single Responsibility with interfaces

--- System Design/SOLID Principle/IUser.cs
+++ System Design/SOLID Principle/IUser.cs
@@ -8,5 +8,18 @@ namespace SOLID_Principle
 {
     internal interface IUser
     {
+        bool Login(string username, string password);
+        bool Register(string username, string password, string email);
+
+    }
+    // Single Responsiblities with multiple interfaces
+    // We can perform this by object creational macaniciasm
+    interface ILogger
+    {
+        void LogError(string error);
+    }
+    interface IEmail
+    {
+        bool SendEmail(string emailContent);
     }
 }
